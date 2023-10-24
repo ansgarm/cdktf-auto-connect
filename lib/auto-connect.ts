93852dafd69200ea1af13a5015fb0314974ec471
Ansgar Mertens <ansgar@hashicorp.com>
2023-10-24 17:15:42 +0200

    add support for resources

--- lib/auto-connect.ts
+++ lib/auto-connect.ts
@@ -2,6 +2,7 @@ import {
   Aspects,
   IAspect,
   TerraformModule,
+  TerraformResource,
   TerraformStack,
 } from "cdktf";
 import { Construct, IConstruct } from "constructs";
@@ -16,8 +17,8 @@ export class CdktfAutoConnect implements IAspect {
   }
 
   visit(node: IConstruct): void {
-    if (node instanceof TerraformModule && connections[node.source]) {
-      const connection = connections[node.source];
+    if (node instanceof TerraformModule && connections[node.source] || (node instanceof TerraformResource && connections[node.terraformResourceType])) {
+      const connection = connections[getConstructId(node)];
       const a = node;
       const b = this.find(connection[0]);
       if (b) {
@@ -26,31 +27,52 @@ export class CdktfAutoConnect implements IAspect {
     }
   }
 
-  private find(
-    terraformModuleSource: TerraformModule["source"]
-  ): TerraformModule | undefined {
+  private find(connectableId: ConnectableId): Connectable | undefined {
     return TerraformStack.of(this.scope)
       .node.findAll()
       .find(
         (c) =>
-          c instanceof TerraformModule && c.source === terraformModuleSource
+          (c instanceof TerraformModule && c.source === connectableId) ||
+          (c instanceof TerraformResource &&
+            c.terraformResourceType === connectableId)
       ) as TerraformModule | undefined;
   }
 }
 
-type ConnectFn<F extends TerraformModule, T extends TerraformModule> = (
+type ConnectableId =
+  | TerraformModule["source"]
+  | TerraformResource["terraformResourceType"];
+type Connectable = TerraformModule | TerraformResource;
+
+type ConnectFn<F extends Connectable, T extends Connectable> = (
   a: F,
   b: T
 ) => void;
 
 const connections: Record<
-  TerraformModule["source"],
-  [TerraformModule["source"], ConnectFn<TerraformModule, TerraformModule>]
+  ConnectableId,
+  [ConnectableId, ConnectFn<Connectable, Connectable>]
 > = {};
 
 export function registerConnection<
-  F extends TerraformModule,
-  T extends TerraformModule
+  F extends Connectable,
+  T extends Connectable
 >(from: Constructor<F>, to: Constructor<T>, connect: ConnectFn<F, T>) {
-  connections[new from().source] = [new to().source, connect as any];
+  connections[getId(from)] = [getId(to), connect as any];
+}
+
+function getConstructId(o: Connectable): ConnectableId {
+  if (o instanceof TerraformModule) {
+    return o.source;
+  } else if (o instanceof TerraformResource) {
+    return o.terraformResourceType;
+  }
+  throw new Error(
+    `Invalid Connectable ${o}. Only Resources and Modules are supported at this time`
+  );
+}
+
+function getId(c: Constructor<Connectable>): ConnectableId {
+  const o = new c();
+  return getConstructId(o);
 }
--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
-import { Construct } from "constructs";
+import { Construct, IConstruct } from "constructs";
 import { App, TerraformStack, Token } from "cdktf";
 import { Aurora } from "./.gen/modules/aurora";
 import { Vpc } from "./.gen/modules/vpc";
+import { RandomProvider } from "./.gen/providers/random/provider";
+import { Pet } from "./.gen/providers/random/pet";
 import { CdktfAutoConnect, registerConnection } from "./lib/auto-connect";
 
 registerConnection(Aurora, Vpc, (db, vpc) => {
@@ -13,10 +15,27 @@ class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
+    new RandomProvider(this, "random");
+
     CdktfAutoConnect.init(this);
 
     new Vpc(this, "vpc");
     new Aurora(this, "db");
+
+    new MyConstruct(this, "construct");
+  }
+}
+
+class MyConstruct extends Construct {
+  constructor(scope: IConstruct, id: string) {
+    super(scope, id);
+
+    new Pet(this, "pet");
+
+    // TODO: allow using the pet instance here instead of the class?
+    registerConnection(Pet, Aurora, (pet, db) => {
+      pet.prefix = db.clusterIdOutput;
+    });
 
   }
 }
